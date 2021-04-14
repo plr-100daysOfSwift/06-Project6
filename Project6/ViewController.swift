529a7694ed1f7a5ee78ef2d8e533aa0781f53bec
Paul Richardson <developer@photographerafoot.com>
2021-04-14 10:05:25 +1000

    CHG: use safeAreaLayoutGuide for trailing and leading anchors

--- Project6/ViewController.swift
+++ Project6/ViewController.swift
@@ -51,8 +51,8 @@ class ViewController: UIViewController {
 		var previous: UILabel?
 
 		for label in [label1, label2, label3, label4, label5] {
-			label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-			label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+			label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+			label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
 			label.heightAnchor.constraint(equalToConstant: 88).isActive = true
 
 			if let previous = previous {
